97c79247c34362608d0b737a41a5f70de45459e8
DarianOthman <darian.othman@gmail.com>
2023-11-29 20:41:47 +0100

    Definition of what_area and what_perimeter + Classes Triangle, Square, and Rectangle

--- calculations.py
+++ calculations.py
@@ -1,7 +1,4 @@
-from abc import ABC
-
-
-class Shape(ABC):
+class Calculations:
     def what_area(self, shapes):
         print(f"Calculating the area")
         print(f"====================")
@@ -17,7 +14,7 @@ class Shape(ABC):
             print(f"Perimeter: {shape.what_perimeter()}")
 
 
-class Triangle(Shape):
+class Area_triangle:
     def __init__(self, base, height, second_side, third_side):
         self.base = base
         self.height = height
@@ -27,28 +24,47 @@ class Triangle(Shape):
     def what_area(self):
         return 0.5 * (self.base * self.height)
 
+
+class Perimeter_triangle:
+    def __init__(self, base, height, second_side, third_side):
+        self.base = base
+        self.height = height
+        self.second_side = second_side
+        self.third_side = third_side
+
     def what_perimeter(self):
         return self.base + self.second_side + self.third_side
 
 
-class Square(Shape):
+class Area_square:
     def __init__(self, side):
         self.side = side
 
     def what_area(self):
         return self.side * self.side
 
+
+class Perimeter_square:
+    def __init__(self, side):
+        self.side = side
+
     def what_perimeter(self):
         return self.side * 4
 
 
-class Rectangle(Shape):
+class Area_rectangle:
     def __init__(self, large, long):
         self.large = large
         self.long = long
 
     def what_area(self):
         return self.large * self.long
+
+
+class Perimeter_rectangle:
+    def __init__(self, large, long):
+        self.large = large
+        self.long = long
 
     def what_perimeter(self):
         return 2 * (self.large + self.long)
