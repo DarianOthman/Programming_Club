5d7bde32a2a28011b13a51e5c067225c05cb1f49
DarianOthman <darian.othman@gmail.com>
2023-11-29 21:18:42 +0100

    Class for all shapes (Accurate)

---
+++ shape.py
@@ -0,0 +1,28 @@
+from calculations import (Area_triangle, Perimeter_triangle, Area_square, Perimeter_square, Area_rectangle,
+                          Perimeter_rectangle)
+
+
+class Shape:
+    def __init__(self, name):
+        self.name = name
+
+
+class Triangle(Shape, Area_triangle, Perimeter_triangle):
+    def __init__(self, name, base, height, second_side, third_side):
+        Area_triangle.__init__(self, base, height, second_side, third_side)
+        Perimeter_triangle.__init__(self, base, height, second_side, third_side)
+        super().__init__(name)
+
+
+class Square(Shape, Area_square, Perimeter_square):
+    def __init__(self, name, side):
+        Area_square.__init__(self, side)
+        Perimeter_square.__init__(self, side)
+        super().__init__(name)
+
+
+class Rectangle(Shape, Area_rectangle, Perimeter_rectangle):
+    def __init__(self, name, large, long):
+        Area_rectangle.__init__(self, large, long)
+        Perimeter_rectangle.__init__(self, large, long)
+        super().__init__(name)
